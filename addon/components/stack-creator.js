4e6ff3a07b299b30df3bf617776a0626d79b9ccb
Nóra Szepes <nora.szepes@tenforce.com>
2017-09-22 11:57:05 +0200

    create stack with either location or repository

--- addon/components/stack-creator.js
+++ addon/components/stack-creator.js
@@ -6,6 +6,7 @@ export default Ember.Component.extend(Validations, {
   stackIcon: Ember.Object.create(),
   stackTitle: null,
   stackDockerFile: null,
+  stackLocation: null,
 
   validations: {
     'stackTitle': {
@@ -13,10 +14,13 @@ export default Ember.Component.extend(Validations, {
     },
     'stackDockerFile': {
       presence: true
+    },
+    'stackLocation': {
+      presence: true
     }
   },
 
-  isInputEmpty: function(inputFieldErrors, cssClasses) {
+  isInputEmpty: function (inputFieldErrors, cssClasses) {
     if (this.get(inputFieldErrors).length > 0) {
       this.set(cssClasses, 'input-required');
       return true;
@@ -27,31 +31,60 @@ export default Ember.Component.extend(Validations, {
 
   inputRequiredTitleCssClasses: "",
   inputRequiredDockerFileCssClasses: "",
+  inputRequiredLocationCssClasses: "",
 
   actions: {
-    create: function() {
+    create: function () {
       //we need to do it like this
       // if i would call it in the if statement, only one would be red at the end
       let isTitleEmpty = this.isInputEmpty('errors.stackTitle', 'inputRequiredTitleCssClasses');
       let isDockerFileEmpty = this.isInputEmpty('errors.stackDockerFile', 'inputRequiredDockerFileCssClasses');
-      if (isTitleEmpty || isDockerFileEmpty) {
-        return;
+      let isLocationEmpty = this.isInputEmpty('errors.stackLocation', 'inputRequiredLocationCssClasses');
+
+
+      // depending on the type we check for:
+      // with Location: title & location
+      //  with docker-file: title & dockerfile
+      if (this.get('createFromLocation') === true) {
+        if (isTitleEmpty || isLocationEmpty) {
+          return;
+        }
+      }
+      else {
+        if (isTitleEmpty || isDockerFileEmpty) {
+          return;
+        }
       }
 
       let stack = this.get('store').createRecord('stack', {
-        dockerFile: this.get('stackDockerFile'),
         title: this.get('stackTitle'),
         icon: this.get('stackIcon.icon'),
       });
+
+      // depending on the type we create a stack with
+      // either a dockerfile or a git repo location
+      if (this.get('createFromLocation') === true) {
+        stack.set('location', this.get('stackLocation'));
+      }
+      else {
+        stack.set('dockerFile', this.get('stackDockerFile'));
+      }
+
       stack.save().then(() => {
         this.set('stackDockerFile', null);
         this.set('stackTitle', '');
+        this.set('stackLocation', '');
+        this.set('inputRequiredLocationCssClasses', "");
+        this.set('inputRequiredTitleCssClasses', "");
+        this.set('inputRequiredDockerFileCssClasses', "");
         return this.set('stackIcon', Ember.Object.create());
       });
     },
-    clear: function() {
+    clear: function () {
       this.set('stackDockerFile', null);
       this.set('stackTitle', "");
+      this.set('stackLocation', "");
+      this.set('inputRequiredLocationCssClasses', "");
       this.set('inputRequiredTitleCssClasses', "");
       this.set('inputRequiredDockerFileCssClasses', "");
       this.set('stackIcon.icon', '');

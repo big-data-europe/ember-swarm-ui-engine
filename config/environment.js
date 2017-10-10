939ac001056ec22a90a05bb1311b68d19fe83697
Nóra Szepes <nora.szepes@tenforce.com>
2017-10-10 16:10:25 +0200

    added search debounce to environment.js

--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,7 @@ module.exports = function(environment) {
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
+    searchDebounceMiliseconds: 250,
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build

df55e431d7f6e84cafc039674c78640ca7cbfd18
Nóra Szepes <nora.szepes@tenforce.com>
2017-10-18 14:32:46 +0200

    now the pipeline-manager knows what is active and what is not

--- addon/components/pipeline-list.js
+++ addon/components/pipeline-list.js
@@ -2,6 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   editable: true,
+  lastClicked: null,
 
   manageButtonText: Ember.computed('editable', function(){
     if(this.get('editable')){
@@ -12,6 +13,14 @@ export default Ember.Component.extend({
   actions: {
     manage:  function() {
       this.toggleProperty('editable');
+    },
+    choosePipeline: function(pipelineId) {
+      if (this.get('lastClicked') === pipelineId) {
+        this.set('lastClicked', null);
+        return false;
+      }
+        this.set('lastClicked', pipelineId);
+        return false;
     }
   }
 });
